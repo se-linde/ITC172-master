3bb272f4ac51941a9215c88b7af1a91842fa7036
Sara Dwayne Linde <lindesara@gmail.com>
2017-01-30 14:54:18 -0800

    Final version of example

---
+++ Assignment3example1/App_Code/BooksClass.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+// call some libraries up here. 
+using System.Data;
+using System.Data.SqlClient; 
+using System.Configuration;
+
+
+/// <summary>
+/// Summary description for BooksClass
+/// </summary>
+public class BooksClass
+{
+
+    private SqlConnection connect; 
+        public BooksClass()
+        {
+
+        string connectString = ConfigurationManager.ConnectionStrings["BookReviewConnection"].ToString();
+        connect = new SqlConnection(connectString);
+
+        }
+
+    public DataTable GetBooks(int authorKey)
+    {
+        DataTable table = new DataTable();
+        string sql = "Select * from Book inner Join AuthorBook " + 
+            "on Book.BookKey = AuthorBook.BookKey " + 
+            "where AuthorKey = @author";
+        SqlCommand cmd = new SqlCommand(sql, connect);
+        cmd.Parameters.AddWithValue("@author", authorKey);
+        SqlDataReader reader = null;
+        connect.Open();
+        reader = cmd.ExecuteReader();
+        table.Load(reader);
+
+        reader.Close();
+        connect.Close();
+        return table; 
+
+    }
+
+    public DataTable GetAuthors()
+    {
+        DataTable table = new DataTable();
+        string sql = "Select Authorkey,AuthorName From Author";
+        SqlCommand cmd = new SqlCommand(sql, connect);
+        SqlDataReader reader = null;
+        connect.Open();
+        reader = cmd.ExecuteReader();
+        table.Load(reader);
+
+        reader.Close();
+        connect.Close();
+        return table;
+    }
+
+}
---
+++ Assignment3example1/GrantRequestBooks.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="GrantRequestBooks.aspx.cs" Inherits="GrantRequestBooks" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Books</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1>Books!</h1>
+        <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="true" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged"></asp:DropDownList>
+        <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+
+    </div>
+    </form>
+</body>
+</html>
