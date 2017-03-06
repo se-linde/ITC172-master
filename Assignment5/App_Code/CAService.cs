c5f5f0f04628cef17b4cd572978ae65a1960638a
Sara Dwayne Linde <lindesara@gmail.com>
2017-03-06 14:52:24 -0800

    Add files via upload

---
+++ Assignment5/App_Code/CAService.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CAService" in code, svc and config file together.
+public class CAService : ICAService
+{
+
+    Community_AssistEntities cae = new Community_AssistEntities(); 
+
+    public bool ApplyForGrant(GrantRequest gr)
+    {
+        bool result = true;
+
+        try
+        {
+            // GrantReview review = new GrantReview();
+            // review.GrantRequest = gr; 
+            // review.GrantRequestStatus = "pending";
+            cae.GrantRequests.Add(gr);
+            // cae.GrantReviews.Add(review);
+            cae.SaveChanges();
+        }
+
+        catch(Exception ex)
+        {
+            result = false;
+            throw ex;
+        }
+
+        return result; 
+
+    }
+
+    public List<GrantInfo> GetGrantsByPerson(int personId) // was GetGrantsByperson
+    {
+        var grants = from g in cae.GrantRequests
+                     from r in g.GrantReviews
+                     where g.PersonKey == personId
+                     select new
+                     {
+                         g.GrantRequestDate,
+                         g.GrantRequestExplanation,
+                         g.GrantType.GrantTypeName,
+                         g.GrantRequestAmount,
+                         r.GrantRequestStatus
+                     };
+        List<GrantInfo> info = new List<GrantInfo>(); 
+        foreach (var gr in grants)
+        {
+            GrantInfo gi = new GrantInfo();
+            gi.GrantTypeName = gr.GrantTypeName; // was granttypesomethingelse
+            gi.Explanation = gr.GrantRequestExplanation;
+            gi.Amount = (decimal)gr.GrantRequestAmount;
+            gi.Status = gr.GrantRequestStatus;
+
+            info.Add(gi); 
+
+        }
+
+        return info; 
+
+    }
+
+    public List<GrantType> GetGrantTypes() // list is like an array, and is a generic. Give it a type at runtime. 
+    {
+
+        var types = from g in cae.GrantTypes
+                     select new
+                     {
+                         g.GrantTypeKey,
+                         g.GrantTypeName
+                     };
+        List<GrantType> gTypes = new List<GrantType>(); 
+
+        foreach(var t in types)
+        {
+            GrantType type = new GrantType();
+            type.GrantTypeKey = t.GrantTypeKey; 
+            type.GrantTypeName = t.GrantTypeName;
+            gTypes.Add(type);  
+        }
+
+        return gTypes;  
+    }
+
+    public int PersonLogin(string user, string password)
+    {
+        int key = 0; 
+        int result = cae.usp_Login(user, password); 
+        if (result != -1)
+        {
+            var uKey = (from p in cae.People
+                       where p.PersonEmail.Equals(user)
+                       select p.PersonKey).FirstOrDefault();
+
+            key = (int)uKey; 
+        }
+
+        return key; 
+    }
+
+    public bool RegisterPerson(PersonLite p)
+    {
+        bool result = true;
+
+        int success = cae.usp_Register(p.LastName, p.FirstName, p.Email,
+            p.Password, p.Apartment, p.Street, p.City, p.State,
+            p.Zipcode, p.HomePhone, p.WorkPhone);
+
+        if (success == -1) // Doesn't have to have brackets if there's only one line/statement. 
+            result = false; 
+
+        return result;
+
+    }
+}
---
+++ Assignment5/App_Code/ICAService.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICAService" in both code and config file together.
+[ServiceContract]
+public interface ICAService
+{
+    [OperationContract]
+    int PersonLogin(string user, string password);
+
+    [OperationContract]
+
+    bool RegisterPerson(PersonLite p);
+
+    [OperationContract]
+
+    bool ApplyForGrant(GrantRequest gr);
+
+    [OperationContract]
+
+    List<GrantType> GetGrantTypes();
+
+    [OperationContract]
+
+    List<GrantInfo> GetGrantsByPerson(int personId); 
+
+}
+
+[DataContract] // compiler directive
+
+public class PersonLite
+{
+    [DataMember]
+    public string LastName { set; get; }
+
+    [DataMember]
+    public string FirstName { set; get; }
+
+    [DataMember]
+    public string Email { set; get; }
+
+    [DataMember]
+    public string Password { set; get; }
+
+    [DataMember]
+    public string Apartment { set; get; }
+
+    [DataMember]
+    public string Street { set; get; }
+
+    [DataMember]
+    public string City { set; get; }
+
+    [DataMember]
+    public string State { set; get; }
+
+    [DataMember]
+    public string Zipcode { set; get; }
+
+    [DataMember]
+    public string HomePhone { set; get; }
+
+    [DataMember]
+    public string WorkPhone { set; get; }
+
+
+
+}
+
+[DataContract]
+
+public class GrantInfo
+{
+    [DataMember]
+    public string GrantTypeName { set; get; }
+
+
+    [DataMember]
+    public string Explanation { set; get; }
+
+
+    [DataMember]
+    public decimal Amount { set; get; }
+
+
+    [DataMember]
+    public string Status { set; get; }
+
+}
