a609cda8a0241f88e3137ad84fed757efda5448a
Sara Dwayne Linde <lindesara@gmail.com>
2017-03-13 10:51:38 -0700

    Add files via upload

---
+++ Assignment7/Models/PersonLite.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment7.Models
+{
+    public class PersonLite
+    {
+        public int PersonKey { set; get; }
+
+        public string LastName { set; get; }
+
+        public string FirstName { set; get; }
+
+        public string Email { set; get; }
+
+        public string Apartment { set; get; }
+
+        public string Street { set; get; }
+
+        public string City { set; get; }
+
+        public string State { set; get; }
+
+        public string Zipcode { set; get; }
+
+        public string HomePhone { set; get; }
+
+        public string WorkPhone { set; get; }
+
+        public string Password { set; get; }
+
+
+    }
+}
