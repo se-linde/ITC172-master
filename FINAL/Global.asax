3a6ad3803cb3e0929aeee994cff5ebdb83df5153
Sara Dwayne Linde <lindesara@gmail.com>
2017-03-15 11:48:26 -0700

    Add files via upload

---
+++ FINAL/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FinalProjV2.MvcApplication" Language="C#" %>
---
+++ FINAL/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinalProjV2.Startup))]
+namespace FinalProjV2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
